c43179246a4c8b183e67ad273c5a42681a4197dc
Anurdha Yeole <108941913+anuradha-yeole@users.noreply.github.com>
2025-11-16 12:25:12 -0500

    Update config.py

--- src/config.py
+++ src/config.py
@@ -1,1 +1,12 @@
+from pathlib import Path
 
+# Paths
+PROJECT_ROOT = Path(__file__).resolve().parents[1]
+DATA_DIR = PROJECT_ROOT / "data"
+MODELS_DIR = PROJECT_ROOT / "models"
+
+RAW_DATA_PATH = DATA_DIR / "casestudy.xlsx"
+PREPROCESSED_CSV_PATH = DATA_DIR / "preprocessed.csv"
+
+BEST_MODEL_PATH = MODELS_DIR / "best_model.joblib"
+LABEL_ENCODER_PATH = MODELS_DIR / "label_encoder.json"
